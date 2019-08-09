e615db1d80034695e58c915a17ad3b3c62390cfc
JoonHo Lee <joonl4@uw.edu>
2019-08-08 23:24:54 -0700

    V9 very short training as it is just finetuning

--- effnet_regression3.py
+++ effnet_regression3.py
@@ -74,7 +74,7 @@ def crop_image_from_gray(img,tol=7):
 
 def load_ben_color(image, sigmaX=10):
     # image = cv2.imread(path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(cv2.UMat(image), cv2.COLOR_BGR2RGB)
     image = crop_image_from_gray(image)
     image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
     image=cv2.addWeighted ( image,4, cv2.GaussianBlur( image , (0,0) , sigmaX) ,-4 ,128)
