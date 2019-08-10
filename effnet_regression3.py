33186f192a285f9d09c518acadb11efe64edd8b6
JoonHo Lee <JoonHoHere@gmail.com>
2019-08-10 17:17:10 +1000

    Think pretraining worked

--- effnet_regression3.py
+++ effnet_regression3.py
@@ -18,10 +18,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.utils import class_weight, shuffle
 from keras.losses import binary_crossentropy, categorical_crossentropy
-from keras.applications.densenet import DenseNet121, DenseNet169, DenseNet201
-from keras.applications.xception import Xception
-from keras.applications.resnet50 import ResNet50
-from efficientnet import EfficientNetB5
+from efficientnet import EfficientNetB4, EfficientNetB5
 import scipy
 from imgaug import augmenters as iaa
 import imgaug as ia
