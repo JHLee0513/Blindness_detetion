d753a6804352595854cbb6864fee2c8f3b8d3574
JHLee0513 <JoonHoHere@gmail.com>
2019-08-20 11:55:19 -0700

    basic aug to finetuning approach

--- effnet_regression_simple_v5_2.py
+++ effnet_regression_simple_v5_2.py
@@ -192,7 +192,6 @@ seq = iaa.Sequential(
         # apply the following augmenters to most images
         iaa.Fliplr(0.5), # horizontally flip 50% of all images
         iaa.Flipud(0.5), # vertically flip 20% of all images
-        sometimes(iaa.size.Crop(percent = (0.05, 0.2), keep_size = True)),
         sometimes(iaa.Affine(
             scale={"x": (0.9, 1.1), "y": (0.9, 1.1)}, # scale images to 80-120% of their size, individually per axis
             # translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)}, # translate by -20 to +20 percent (per axis)
@@ -201,50 +200,9 @@ seq = iaa.Sequential(
             # order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
             cval=(0, 255), # if mode is constant, use a cval between 0 and 255
             mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
-        ))
-        # execute 0 to 5 of the following (less important) augmenters per image
-        # don't execute all of them, as that would often be way too strong
-        # ,iaa.SomeOf((0, 5),
-        #     [
-        #         sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
-        #         iaa.OneOf([
-        #             iaa.GaussianBlur((0, 1.0)), # blur images with a sigma between 0 and 3.0
-        #             iaa.AverageBlur(k=(3, 5)), # blur image using local means with kernel sizes between 2 and 7
-        #             iaa.MedianBlur(k=(3, 5)), # blur image using local medians with kernel sizes between 2 and 7
-        #         ]),
-        #         iaa.Sharpen(alpha=(0, 1.0), lightness=(0.9, 1.1)), # sharpen images
-        #         iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
-        #         # search either for all edges or for directed edges,
-        #         # blend the result with the original image using a blobby mask
-        #         # iaa.SimplexNoiseAlpha(iaa.OneOf([
-        #         #     iaa.EdgeDetect(alpha=(0.5, 1.0)),
-        #         #     iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
-        #         # ])),
-        #         iaa.GammaContrast((0.75, 1.25)),
-        #         iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.01*255), per_channel=0.5), # add gaussian noise to images
-        #         # iaa.OneOf([
-        #         #     iaa.Dropout((0.01, 0.05), per_channel=0.5), # randomly remove up to 10% of the pixels
-        #         #     iaa.CoarseDropout((0.01, 0.03), size_percent=(0.01, 0.02), per_channel=0.2),
-        #         # ]),
-        #         # iaa.Invert(0.01, per_channel=True), # invert color channels
-        #         # iaa.Add((-2, 2), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
-        #         # iaa.AddToHueAndSaturation((-1, 1)), # change hue and saturation
-        #         # either change the brightness of the whole image (sometimes
-        #         # per channel) or change the brightness of subareas
-        #         # iaa.OneOf([
-        #         #     iaa.Multiply((0.9, 1.1), per_channel=0.5),
-        #         #     iaa.FrequencyNoiseAlpha(
-        #         #         exponent=(-1, 0),
-        #         #         first=iaa.Multiply((0.9, 1.1), per_channel=True),
-        #         #         second=iaa.ContrastNormalization((0.9, 1.1))
-        #         #     )
-        #         # ]),
-        #         # # sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-        #         # # sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
-        #         # sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
-        #     ],
-        #     random_order=True
-        # )
+        )),
+        sometimes(iaa.size.Crop(percent = (0.05, 0.2), keep_size = True)),
+        sometimes(iaa.contrast.LinearContrast(alpha = (0.9, 1.1)))
     ],
     random_order=True)
 
@@ -265,7 +223,7 @@ train_x = train['id_code']
 train_y = train['diagnosis'].astype(int)
 val_x = val['id_code']
 val_y = val['diagnosis'].astype(int)
-train_generator = My_Generator(train_x, train_y, batch, is_train=True, augment=False)
+train_generator = My_Generator(train_x, train_y, batch, is_train=True, augment=True)
 val_generator = My_Generator(val_x, val_y, batch, is_train=False)
 qwk = QWKEvaluation(validation_data=(val_generator, val_y),
                     batch_size=batch, interval=1)
@@ -273,23 +231,23 @@ model = build_model(freeze = False)
 model.load_weights("/nas-homes/joonl4/blind_weights/raw_effnet_pretrained_regression_fold_v110.hdf5")
 save_model_name = '/nas-homes/joonl4/blind_weights/snap_trash.hdf5'
 
-for cv_index in range(3):
+for cv_index in range(1):
     if cv_index != 0:
         model.load_weights(save_model_name)
     model.compile(loss='mse', optimizer = Adam(lr=1e-3),
                 metrics= ['accuracy'])
-    cycle = len(train_y)/batch * 4
+    cycle = len(train_y)/batch * 10
     cyclic = CyclicLR(mode='exp_range', base_lr = 1e-4, max_lr = 1e-3, step_size = cycle)
     model_checkpoint = ModelCheckpoint(save_model_name,monitor= 'val_loss',
                                 mode = 'min', save_best_only=True, verbose=1,save_weights_only = True)
     model.fit_generator(
         train_generator,
         steps_per_epoch=len(train_y)/batch,
-        epochs=4,
+        epochs=10,
         verbose = 1,
         callbacks = [qwk, cyclic, model_checkpoint],
         validation_data = val_generator,
         validation_steps = len(val_y)/batch,
         workers=1, use_multiprocessing=False)
     model.load_weights(save_model_name)
-    model.save("/nas-homes/joonl4/blind_weights/raw_effnet_pretrained_regression_fold_v14_2_snap"+str(cv_index+1)+".h5")
+    model.save("/nas-homes/joonl4/blind_weights/raw_effnet_pretrained_regression_fold_v14_3.h5")
