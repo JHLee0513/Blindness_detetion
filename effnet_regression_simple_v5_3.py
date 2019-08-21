98f067a58ad4c3b3dbbfaea95a193fd3c05ee1f1
JoonHo Lee <joonl4@uw.edu>
2019-08-20 23:50:45 -0700

    V15 longer training

--- effnet_regression_simple_v5_3.py
+++ effnet_regression_simple_v5_3.py
@@ -265,25 +265,60 @@ qwk = QWKEvaluation(validation_data=(val_generator, val_y),
                     batch_size=batch, interval=1)
 model = build_model(freeze = False)
 model.load_weights("/nas-homes/joonl4/blind_weights/raw_effnet_pretrained_regression_fold_v110_2.hdf5")
-save_model_name = '/nas-homes/joonl4/blind_weights/snap.hdf5'
+save_model_name = '/nas-homes/joonl4/blind_weights/snapp.hdf5'
 
-for cv_index in range(3):
-    if cv_index != 0:
-        model.load_weights(save_model_name)
-    model.compile(loss='mse', optimizer = SGD(lr=1e-3),
-                metrics= ['accuracy'])
-    cycle = len(train_y)/batch * 8
-    cyclic = CyclicLR(mode='exp_range', base_lr = 1e-4, max_lr = 1e-3, step_size = cycle)
-    model_checkpoint = ModelCheckpoint(save_model_name,monitor= 'val_loss',
-                                mode = 'min', save_best_only=True, verbose=1,save_weights_only = True)
-    model.fit_generator(
-        train_generator,
-        steps_per_epoch=len(train_y)/batch,
-        epochs=8,
-        verbose = 1,
-        callbacks = [qwk, cyclic, model_checkpoint],
-        validation_data = val_generator,
-        validation_steps = len(val_y)/batch,
-        workers=1, use_multiprocessing=False)
-    model.load_weights(save_model_name)
-    model.save("/nas-homes/joonl4/blind_weights/raw_effnet_pretrained_regression_fold_v15_snap"+str(cv_index+1)+".h5")
+model.compile(loss='mse', optimizer = SGD(lr=1e-3),
+            metrics= ['accuracy'])
+cycle = len(train_y)/batch * 5
+cyclic = CyclicLR(mode='exp_range', base_lr = 1e-4, max_lr = 1e-3, step_size = cycle)
+model_checkpoint = ModelCheckpoint(save_model_name,monitor= 'val_loss',
+                            mode = 'min', save_best_only=True, verbose=1,save_weights_only = True)
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=len(train_y)/batch,
+    epochs=5,
+    verbose = 1,
+    callbacks = [qwk, cyclic, model_checkpoint],
+    validation_data = val_generator,
+    validation_steps = len(val_y)/batch,
+    workers=1, use_multiprocessing=False)
+
+img_target = 512
+SIZE = 512
+IMG_SIZE = 512
+batch = 4
+
+model = build_model(freeze = True)
+model.load_weights(save_model_name)
+model.compile(loss='mse', optimizer = SGD(lr=1e-4),
+            metrics= ['accuracy'])
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=len(train_y)/batch,
+    epochs=3,
+    verbose = 1,
+    callbacks = [qwk, cyclic, model_checkpoint],
+    validation_data = val_generator,
+    validation_steps = len(val_y)/batch,
+    workers=1, use_multiprocessing=False)
+
+model = build_model(freeze = False)
+model.load_weights(save_model_name)
+model.compile(loss='mse', optimizer = SGD(lr=1e-3),
+            metrics= ['accuracy'])
+
+cycle = len(train_y)/batch * 4
+cyclic = CyclicLR(mode='exp_range', base_lr = 1e-5, max_lr = 1e-4, step_size = cycle)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=len(train_y)/batch,
+    epochs=4,
+    verbose = 1,
+    callbacks = [qwk, cyclic, model_checkpoint],
+    validation_data = val_generator,
+    validation_steps = len(val_y)/batch,
+    workers=1, use_multiprocessing=False)
+
+model.save("/nas-homes/joonl4/blind_weights/raw_effnet_pretrained_regression_fold_v16.h5")
