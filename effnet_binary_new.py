07883e5d395f6ba8e4bd211656fc2d0572968a24
JoonHo Lee <joonl4@uw.edu>
2019-08-24 21:43:57 -0700

    pretraining B4 again

--- effnet_binary_new.py
+++ effnet_binary_new.py
@@ -362,7 +362,6 @@ for cv_index in range(1):
         steps_per_epoch=2560/batch,
         epochs=75,
         verbose = 1,
-        initial_epoch = 41,
         callbacks = [model_checkpoint, qwk, cyclic],
         validation_data = val_generator,
         validation_steps = 1100/batch,
