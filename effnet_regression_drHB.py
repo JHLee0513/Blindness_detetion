64f5d50928be89cf3f28b6f3c2bdceb71c8e8fad
JoonHo Lee <joonl4@uw.edu>
2019-08-28 00:44:42 -0700

    drhb script uses full data

--- effnet_regression_drHB.py
+++ effnet_regression_drHB.py
@@ -178,44 +178,13 @@ class QWKEvaluation(Callback):
                 # self.model.save(qwk_ckpt_name)
                 #log.write(str(log_fold) + ": " + str(score) + "\n")
 
-
 class AltModelCheckpoint(Callback):
-    """Save the model after every epoch.
 
-    `filepath` can contain named formatting options,
-    which will be filled with the values of `epoch` and
-    keys in `logs` (passed in `on_epoch_end`).
-
-    For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
-    then the model checkpoints will be saved with the epoch number and
-    the validation loss in the filename.
-
-    # Arguments
-        filepath: string, path to save the model file.
-        monitor: quantity to monitor.
-        verbose: verbosity mode, 0 or 1.
-        save_best_only: if `save_best_only=True`,
-            the latest best model according to
-            the quantity monitored will not be overwritten.
-        mode: one of {auto, min, max}.
-            If `save_best_only=True`, the decision
-            to overwrite the current save file is made
-            based on either the maximization or the
-            minimization of the monitored quantity. For `val_acc`,
-            this should be `max`, for `val_loss` this should
-            be `min`, etc. In `auto` mode, the direction is
-            automatically inferred from the name of the monitored quantity.
-        save_weights_only: if True, then only the model's weights will be
-            saved (`model.save_weights(filepath)`), else the full model
-            is saved (`model.save(filepath)`).
-        period: Interval (number of epochs) between checkpoints.
-    """
-
-    def __init__(self, filepath, monitor='val_loss', verbose=0,
-                save_best_only=False, save_weights_only=False,
-                mode='auto', period=1,
-                multiple_gpu=False, name_of_model=None):
-        super(ModelCheckpoint, self).__init__()
+    def __init__(self, filepath, base_model, monitor='val_loss', verbose=0,
+                 save_best_only=False, save_weights_only=False,
+                 mode='auto', period=1):
+        super(MultiGPU_Checkpoint_Callback, self).__init__()
+        self.base_model = base_model
         self.monitor = monitor
         self.verbose = verbose
         self.filepath = filepath
@@ -223,13 +192,11 @@ class AltModelCheckpoint(Callback):
         self.save_weights_only = save_weights_only
         self.period = period
         self.epochs_since_last_save = 0
-        self.multi_gpu_mode = multiple_gpu
-        self.name_of_model = name_of_model
 
         if mode not in ['auto', 'min', 'max']:
             warnings.warn('ModelCheckpoint mode %s is unknown, '
-                        'fallback to auto mode.' % (mode),
-                        RuntimeWarning)
+                          'fallback to auto mode.' % (mode),
+                          RuntimeWarning)
             mode = 'auto'
 
         if mode == 'min':
@@ -256,30 +223,30 @@ class AltModelCheckpoint(Callback):
                 current = logs.get(self.monitor)
                 if current is None:
                     warnings.warn('Can save best model only with %s available, '
-                                'skipping.' % (self.monitor), RuntimeWarning)
+                                  'skipping.' % (self.monitor), RuntimeWarning)
                 else:
                     if self.monitor_op(current, self.best):
                         if self.verbose > 0:
-                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
-                                ' saving model to %s'
-                                % (epoch + 1, self.monitor, self.best,
-                                    current, filepath))
+                            print('Epoch %05d: %s improved from %0.5f to %0.5f,'
+                                  ' saving model to %s'
+                                  % (epoch + 1, self.monitor, self.best,
+                                     current, filepath))
                         self.best = current
                         if self.save_weights_only:
-                            self.model.save_weights(filepath, overwrite=True, multiple_gpu=self.multi_gpu_mode, name_of_model=self.name_of_model)
+                            self.base_model.save_weights(filepath, overwrite=True)
                         else:
-                            self.model.save(filepath, overwrite=True)
+                            self.base_model.save(filepath, overwrite=True)
                     else:
                         if self.verbose > 0:
-                            print('\nEpoch %05d: %s did not improve from %0.5f' %
-                                (epoch + 1, self.monitor, self.best))
+                            print('Epoch %05d: %s did not improve' %
+                                  (epoch + 1, self.monitor))
             else:
                 if self.verbose > 0:
-                    print('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath))
+                    print('Epoch %05d: saving model to %s' % (epoch + 1, filepath))
                 if self.save_weights_only:
-                    self.model.save_weights(filepath, overwrite=True)
+                    self.base_model.save_weights(filepath, overwrite=True)
                 else:
-                    self.model.save(filepath, overwrite=True)
+                    self.base_model.save(filepath, overwrite=True)
 
 sometimes = lambda aug: iaa.Sometimes(0.3, aug)
 
