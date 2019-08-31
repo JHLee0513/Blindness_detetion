88d1e6db0f7b35c0fbd5ae3e03eee1ce076cd815
JoonHo Lee <joonl4@uw.edu>
2019-08-30 21:04:31 -0700

    adversarial training with B1

--- effnet_adv_training_finetune.py
+++ effnet_adv_training_finetune.py
@@ -39,6 +39,9 @@ val_2019_list = val_2019_list[val_2019_list['name'].str.contains(".png")]
 val = train_df[train_df['id_code'].isin(val_2019_list)]
 train = train_df[~train_df['id_code'].isin(val_2019_list)]
 
+print(val.head())
+print(train.head())
+
 #https://www.kaggle.com/ratthachat/aptos-updatedv14-preprocessing-ben-s-cropping#3.-Further-improve-by-auto-cropping
 
 def crop_image1(img,tol=7):
